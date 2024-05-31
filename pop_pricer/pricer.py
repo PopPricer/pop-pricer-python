f3b3bea66782ba438197d34e70c57e447575d8dc
David LoBosco <5651124+dqlobo@users.noreply.github.com>
2024-05-31 14:47:35 -0600

    add serializer

--- pop_pricer/pricer.py
+++ pop_pricer/pricer.py
@@ -1,7 +1,8 @@
 from datetime import datetime, UTC
 from math import floor
+import json
 
-class PopPricer:
+class Pricer:
   def __init__(self, start_price, max_price, reset_rate=0.05, cost_weight=0.001):
     """
     """
@@ -23,6 +24,7 @@ class PopPricer:
     Check current price for a given cost, to be combined with the cost weight as a multiplier of original price
     """
     return self._push_cost(cost)
+
 
   def _push_cost(self, cost):
     """
@@ -51,5 +53,31 @@ class PopPricer:
       return floor(price) + 0.49
     else:
       return floor(price) + 0.99
+  
+  def __str__(self):
+    return self.Serializer(self).serialize()
 
+  class Serializer:
+    def __init__(self, pricer):
+      self._pricer = pricer
 
+    def serialize(self):
+      data = {
+        "_start_price": self._pricer._start_price,
+        "_max_price": self._pricer._max_price,
+        "_reset_rate": self._pricer._reset_rate,
+        "_cost_weight": self._pricer._cost_weight,
+        "_last_cost": self._pricer._last_cost and self._pricer._last_cost.isoformat(),
+        "_cost": self._pricer._cost
+      }
+      return json.dumps(data)
+
+    @staticmethod
+    def deserialize(str):
+      try:
+        data = json.loads(str)
+        p = Pricer(data['_start_price'], data['_max_price'], data['_reset_rate'], data['_cost_weight'])
+        p._last_cost = data['_last_cost']
+        p._cost = data['_cost']
+      except:
+        return None
